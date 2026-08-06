7093cd3d9c84fb44de632459443509cb259c40c1
irynayaroshyk2019-hub <irynayaroshyk2019@gmail.com>
2026-08-06 23:09:56 +0300

    server.py

--- server.py
+++ server.py
@@ -2,20 +2,54 @@ from flask import Flask, request, jsonify
 
 app = Flask("server")
 
+users = {}
 messages = []
+
 
 @app.route("/")
 def home():
     return "Messenger server працює"
 
+
+@app.route("/register", methods=["POST"])
+def register():
+
+    data = request.json
+
+    phone = data["phone"]
+
+    users[phone] = {
+        "phone": phone
+    }
+
+    return jsonify({
+        "status": "registered",
+        "phone": phone
+    })
+
+
 @app.route("/send", methods=["POST"])
 def send():
-    data = request.json
-    messages.append(data)
-    return jsonify({"status": "sent"})
 
-@app.route("/messages")
-def get_messages():
-    return jsonify(messages)
+    data = request.json
+
+    messages.append(data)
+
+    return jsonify({
+        "status": "sent"
+    })
+
+
+@app.route("/messages/<phone>")
+def get_messages(phone):
+
+    result = []
+
+    for msg in messages:
+        if msg["to"] == phone:
+            result.append(msg)
+
+    return jsonify(result)
+
 
 app.run(host="0.0.0.0", port=5000)
